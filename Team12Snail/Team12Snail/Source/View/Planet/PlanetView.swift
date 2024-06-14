2a1a04c2593e730708bd942cb25fd069cfec9624
YunHak Lee <lyh990408@gmail.com>
2024-06-15 07:13:23 +0900

    [Design] #7 - 홈 화면 가볍게 구현 완료. Layout이 정확하진 않음

--- Team12Snail/Team12Snail/Source/View/Planet/PlanetView.swift
+++ Team12Snail/Team12Snail/Source/View/Planet/PlanetView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct PlanetView: View {
+    @State var selectedIndex: Int = 0
+    
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottom) {
             PlanetImage
+            StarSelectingView(selectedIndex: $selectedIndex)
+                .padding(.bottom, 30)
+                .overlay {
+                   ChangeStarButton
+                }
+        }
+        .overlay(alignment: .topTrailing) {
+            ShoppingButton
+                .padding()
         }
     }
 }
@@ -22,6 +33,45 @@ extension PlanetView {
             .resizable()
             .scaledToFill()
             .clipped()
+    }
+    
+    var ChangeStarButton: some View {
+        HStack {
+            Button {
+                if selectedIndex > 0 {
+                    selectedIndex -= 1
+                }
+                print(selectedIndex)
+            } label: {
+                Image(systemName: "chevron.left")
+                    .font(.largeTitle)
+                    .shadow(radius: 4, y: 4)
+            }
+            .tint(.white)
+            Spacer()
+            Button {
+                if selectedIndex < 3 {
+                    selectedIndex += 1
+                }
+            } label: {
+                Image(systemName: "chevron.right")
+                    .font(.largeTitle)
+                    .shadow(radius: 4, y: 4)
+            }
+            .tint(.white)
+        }
+        .padding(16)
+    }
+    
+    var ShoppingButton: some View {
+        Button {
+            print("move To shop")
+        } label: {
+            Image(systemName: "storefront.circle.fill")
+                .resizable()
+                .frame(width: 48, height: 48)
+                .foregroundStyle(.black)
+        }
     }
 }
 
