7fdf7eba149a1d91a97717efa19a92a2026a1af2
heracloud <heracloud7@naver.com>
2024-06-15 07:56:42 +0900

    #11 Feat/타이머 기능 구현 시작,중지,완료시 별사탕 계산

---
+++ Team12Snail/Team12Snail/Source/View/Planet/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  Team12Snail
+//
+//  Created by 임유리 on 6/15/24.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
